1fdefcf2d2be9845e90cc688efb293f1aeb71d76
jerollins <jrollins33@gatech.edu>
2022-09-01 17:16:56 -0400

    Basic amplify setup

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "photosharingc17d2ac3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "api": {
+        "photosharing": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "storage": {
+        "default": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
